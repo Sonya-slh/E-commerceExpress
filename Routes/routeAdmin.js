6383dc40ecc5d445a7bdd7d565684ec4f6aac8ed
SOUALHYA Sonya <soualhyasonya@gmail.com>
2025-09-10 12:10:16 +0200

    some modification

--- Routes/routeAdmin.js
+++ Routes/routeAdmin.js
@@ -1,6 +1,7 @@
-const controllerAdmin=require("../Controllers/controllerAdmin")
+const controllerAdmin = require("../Controllers/controllerAdmin")
+const verifyCsrf = require("../middleware/verifyCsrf")
 const route=require("express").Router()
 route.post('/add',controllerAdmin.createAdmin)
-route.get('/list',controllerAdmin.listAdmin)
+route.get('/list',verifyCsrf,controllerAdmin.listAdmin)
 route.delete('/delete/:id',controllerAdmin.deleteAdmin)
 module.exports=route;
